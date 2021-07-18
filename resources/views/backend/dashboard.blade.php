f07f441df875f541eaa6f31460b8d976c3914afa
@oong26 <37205723+oong26@users.noreply.github.com>
2021-07-18 22:47:32 +0700

    Update dashboard.blade.php

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -7,7 +7,7 @@
     <div class="page-breadcrumb bg-white">
         <div class="row align-items-center">
             <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                <h4 class="page-title">Blank Page</h4>
+                <h4 class="page-title">Dashboard</h4>
             </div>
             <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                 <div class="d-md-flex">
@@ -29,10 +29,104 @@
         <!-- ============================================================== -->
         <!-- Start Page Content -->
         <!-- ============================================================== -->
-        <div class="row">
-            <div class="col-md-12">
-                <div class="white-box">
-                    <h3 class="box-title">Blank Page</h3>
+        <div class="row justify-content-start">
+            <div class="col-lg-4 col-md-12">
+                <div class="white-box analytics-info">
+                    <h3 class="box-title">Total Admin</h3>
+                    <ul class="list-inline two-part d-flex align-items-center mb-0">
+                        <li>
+                            <div id="sparklinedash"><canvas width="67" height="30"
+                                    style="display: inline-block; width: 67px; height: 30px; vertical-align: top;"></canvas>
+                            </div>
+                        </li>
+                        @php
+                            $totalAdmin = App\Models\User::count();
+                        @endphp
+                        <li class="ms-auto"><span class="counter text-success">{{ $totalAdmin }}</span></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <div class="white-box analytics-info">
+                    <h3 class="box-title">Total Android User</h3>
+                    <ul class="list-inline two-part d-flex align-items-center mb-0">
+                        <li>
+                            <div id="sparklinedash2"><canvas width="67" height="30"
+                                    style="display: inline-block; width: 67px; height: 30px; vertical-align: top;"></canvas>
+                            </div>
+                        </li>
+                        @php
+                            $totalAndroidUser = App\Models\AndroidUsers::count();
+                        @endphp
+                        <li class="ms-auto"><span class="counter text-success">{{ $totalAndroidUser }}</span></li>                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <div class="white-box analytics-info">
+                    <h3 class="box-title">Total Produk</h3>
+                    <ul class="list-inline two-part d-flex align-items-center mb-0">
+                        <li>
+                            <div id="sparklinedash3"><canvas width="67" height="30"
+                                    style="display: inline-block; width: 67px; height: 30px; vertical-align: top;"></canvas>
+                            </div>
+                        </li>
+                        @php
+                            $totalProduk = App\Models\Products::count();
+                        @endphp
+                        <li class="ms-auto"><span class="counter text-success">{{ $totalProduk }}</span></li>                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <div class="white-box analytics-info">
+                    <h3 class="box-title">Total Resep</h3>
+                    <ul class="list-inline two-part d-flex align-items-center mb-0">
+                        <li>
+                            <div id="sparklinedash3"><canvas width="67" height="30"
+                                    style="display: inline-block; width: 67px; height: 30px; vertical-align: top;"></canvas>
+                            </div>
+                        </li>
+                        @php
+                            $totalResep = App\Models\Recipe::count();
+                        @endphp
+                        <li class="ms-auto"><span class="counter text-success">{{ $totalResep }}</span></li>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <div class="white-box analytics-info">
+                    <h3 class="box-title">Total Bahan-bahan</h3>
+                    <ul class="list-inline two-part d-flex align-items-center mb-0">
+                        <li>
+                            <div id="sparklinedash3"><canvas width="67" height="30"
+                                    style="display: inline-block; width: 67px; height: 30px; vertical-align: top;"></canvas>
+                            </div>
+                        </li>
+                        @php
+                            $totalBahan = App\Models\Ingredients::count();
+                        @endphp
+                        <li class="ms-auto"><span class="counter text-success">{{ $totalBahan }}</span></li>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <div class="white-box analytics-info">
+                    <h3 class="box-title">Total Favorit Resep</h3>
+                    <ul class="list-inline two-part d-flex align-items-center mb-0">
+                        <li>
+                            <div id="sparklinedash3"><canvas width="67" height="30"
+                                    style="display: inline-block; width: 67px; height: 30px; vertical-align: top;"></canvas>
+                            </div>
+                        </li>
+                        @php
+                            $totalFavorit = App\Models\Favorit::count();
+                        @endphp
+                        <li class="ms-auto"><span class="counter text-success">{{ $totalFavorit }}</span></li>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
